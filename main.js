c5110f8311ee5b2bffb5fcdaa83b8a0c4d3f7124
Yunyun Liu <celeph4is@gmail.com>
2021-09-13 15:29:53 -0700

    get values from entry form

--- main.js
+++ main.js
@@ -6,13 +6,26 @@ const $submitBtn = document.querySelector('.btn-submit');
 const $entryForm = document.querySelector('.entry-form');
 const $daySelect = document.querySelector('.day-select');
 const $timeSelect = document.querySelector('.time-select');
+const $description = document.querySelector('.description');
 
 $addEntry.addEventListener('click', () => {
   $entryFormModal.showModal();
 });
 
-$entryForm.addEventListener('submit', () => {
+function handleSubmit(e) {
+  preventDefault();
   var selectedDayText = $daySelect.options[$daySelect.selectedIndex].text;
   var selectedTimeText = $timeSelect.options[$timeSelect.selectedIndex].text;
   var selectedDayValue = $daySelect.options[$daySelect.selectedIndex].value;
-})
+  const entryDescription = $entryForm.elements.description.value;
+  console.log('entryDescription', entryDescription)
+  // var entry = {
+  //   day: selectedDayText,
+  //   time: selectedTimeText,
+  //   description: '   ',
+  //   nextEntryId: data.nextEntryId,
+  //   entryId: data.nextEntryId - 1
+  // }
+}
+
+$entryForm.addEventListener('submit', (e) => handleSubmit(e));
