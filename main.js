f0b049ddbc03b9a592a31da1caf1055855297611
Greggory Garceau <ggarceau212@gmail.com>
2021-09-13 18:07:37 -0400

    Added variables for option selector, and changed data model

--- main.js
+++ main.js
@@ -6,14 +6,13 @@ const $submitBtn = document.querySelector('.btn-submit');
 const $entryForm = document.querySelector('.entry-form');
 const $daySelect = document.querySelector('.day-select');
 const $timeSelect = document.querySelector('.time-select');
-var selectedDay = $daySelect.options[$daySelect.selectedIndex].text;
-console.log(selectedDay);
 
 $addEntry.addEventListener('click', () => {
   $entryFormModal.showModal();
 });
 
 $entryForm.addEventListener('submit', () => {
-  var selectedDay = $daySelect.options[$daySelect.selectedIndex].text;
-  var selectedTime = $timeSelect.options[$timeSelect.selectedIndex].text;
+  var selectedDayText = $daySelect.options[$daySelect.selectedIndex].text;
+  var selectedTimeText = $timeSelect.options[$timeSelect.selectedIndex].text;
+  var selectedDayValue = $daySelect.options[$daySelect.selectedIndex].value;
 })
