c5765de4907712220903337d4de080b6a54727fc
Greggory Garceau <ggarceau212@gmail.com>
2021-09-13 17:54:22 -0400

    Added entry form submit listener for select elements

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
+/* global data */
+/* exported data */
 const $addEntry = document.querySelector('.add-entry');
 const $entryFormModal = document.querySelector('dialog');
+const $submitBtn = document.querySelector('.btn-submit');
+const $entryForm = document.querySelector('.entry-form');
+const $daySelect = document.querySelector('.day-select');
+const $timeSelect = document.querySelector('.time-select');
+var selectedDay = $daySelect.options[$daySelect.selectedIndex].text;
+console.log(selectedDay);
+
 $addEntry.addEventListener('click', () => {
   $entryFormModal.showModal();
 });
+
+$entryForm.addEventListener('submit', () => {
+  var selectedDay = $daySelect.options[$daySelect.selectedIndex].text;
+  var selectedTime = $timeSelect.options[$timeSelect.selectedIndex].text;
+})
